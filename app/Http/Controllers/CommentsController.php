4bae40d0c8b3d2ad9722e59fb668df3223f2a637
Алексей Игнатенко <ignatenkolesha123@gmail.com>
2023-11-21 18:30:28 +0100

    some change

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -2,11 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CommentRequest;
 use Illuminate\Http\Request;
 
 class CommentsController extends Controller
 {
     function index(){
         return view('main');
+    }
+
+    function store(CommentRequest $request){
+//        $rules = ['captcha' => 'required|captcha'];
+//        $validator = validator()->make(request()->all(), $rules);
+//        if ($validator->fails()) {
+//            echo '<p style="color: #ff0000;">Incorrect!</p>';
+//        } else {
+//            echo '<p style="color: #00ff30;">Matched :)</p>';
+//        }
+        $this->validate();
+        return $request->all();
     }
 }
