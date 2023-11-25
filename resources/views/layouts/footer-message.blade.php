93c3c1af3626effb67ea1361392da5bfa6b9b261
Алексей Игнатенко <ignatenkolesha123@gmail.com>
2023-11-25 01:57:53 +0100

    footer view

--- resources/views/layouts/footer-message.blade.php
+++ resources/views/layouts/footer-message.blade.php
@@ -1,5 +1,5 @@
 {{--<?php include($_SERVER['DOCUMENT_ROOT'].'/php/captcha.php') ?>--}}
-<div class="footer">
+<div class="footer" id="footer">
     <div class="d-flex justify-content-end w-100">
         <div class="mb-1" style="margin-right: 70px; margin-top: 10px;">
             <button onclick="collapseFooter('form-footer');">
@@ -10,7 +10,10 @@
         </div>
     </div>
     <form action="{{ route("main-post") }}" method="POST" class="main-form-add-comment visible" id="form-footer">
-        @csrf
+        <div id="success_message">
+
+        </div>
+        <input type="hidden" id="token" value="{{ csrf_token() }}">
         @if ($errors->any())
             <div class="alert alert-danger alert-dismissible mb-2 fade show" role="alert">
                 <ul style="list-style-type: none; margin-bottom: 0">
@@ -35,24 +38,28 @@
                                class="form-control @error('username') is-invalid @enderror"
                                id="username" aria-describedby="usernameHelp" placeholder="User Name"
                                required>
+                        <div id="usernameError" class="form-text under_error"></div>
                         <div id="usernameHelp" class="form-text">Numbers and letters of the Latin alphabet.</div>
                     </div>
                     <div style="width: 32.3%; margin-left: 1.5%">
                         <input type="email"
                                class="form-control @error('email') is-invalid @enderror" name="email" required
-                               id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email address">
+                               id="email" aria-describedby="emailHelp" placeholder="Email address">
+                        <div id="emailError" class="form-text under_error"></div>
                         <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
                     </div>
                     <div style="width: 32.3%; margin-left: 1.5%">
                         <input type="text"
                                class="form-control @error('urlHomePage') is-invalid @enderror" name="urlHomePage"
                                id="homepage" aria-describedby="homepageHelp" placeholder="Home page">
+                        <div id="homepageError" class="form-text under_error"></div>
                         <div id="homepageHelp" class="form-text">Your own url.</div>
                     </div>
                 </div>
                 <div class="mb-3">
                     <input class="form-control" type="file" id="attachmentFiles" name="attachmentFiles" style="color: var(--text-color);"
                     onchange="uploadFile();">
+                    <div id="attachmentFilesError" class="form-text under_error"></div>
                 </div>
                 <div>
                     <div id="imagePreview" class="imagePreview">
@@ -60,15 +67,17 @@
                     </div>
                 </div>
                 <div class="mb-3">
-{{--                    <textarea class="form-control @error('comment') is-invalid @enderror" style="height: 100px;" name="comment"--}}
-{{--                              id="comment" aria-describedby="homepageHelp" placeholder="Your comment"></textarea>--}}
+                    <textarea class="form-control @error('comment') is-invalid @enderror" style="height: 100px;" name="comment"
+                              id="comment" aria-describedby="homepageHelp" placeholder="Your comment"></textarea>
+                    <div id="commentError" class="form-text under_error"></div>
 {{--                    <div contenteditable="true" class="form-control" id="comment" style="height: 70px;">--}}
 {{--                    </div>--}}
-                    <div id="comment" contenteditable="true" class="form-control mb-1" style="height: 100px;"></div>
-                    <div id="evalResult"></div>
+{{--                    <div id="comment" contenteditable="true" class="form-control mb-1" style="height: 100px;"></div>--}}
+                    <div id="checkResult" class="check-result mt-2"></div>
+                    <div id="invisibleBlock" style="display: none"></div>
                 </div>
                 <div class="d-flex flex-row gap-2">
-                    <div class="form-text" style="font-size: 15px;">You can use this tags: </div>
+                    <div class="form-text under_error" style="font-size: 15px;">You can use this tags: </div>
                     <button type="button" class="button button-tags" onclick="addTag('a');">a</button>
                     <button type="button" class="button button-tags" onclick="addTag('code');">code</button>
                     <button type="button" class="button button-tags" onclick="addTag('i');">i</button>
@@ -77,14 +86,15 @@
             </div>
             <div class="d-flex flex-column comment-container-right-side">
                 <div>
-                    <input type="text" name="captcha" class="form-control @error('captcha') is-invalid @enderror"
+                    <input type="text" name="captcha" id="captcha" class="form-control @error('captcha') is-invalid @enderror"
                            required placeholder="Captcha" />
-                    <div class="captcha-container">
+                    <div id="captchaError" class="form-text under_error"></div>
+                    <div class="captcha-container" id="captcha-container">
                         {!! captcha_img() !!}
                     </div>
                 </div>
                 <div class="d-flex justify-content-center align-items-center button-send">
-                    <button type="submit">
+                    <button type="button" id="submit">
                         <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="var(--button-background-color)" class="bi bi-send" viewBox="0 0 16 16">
                             <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z"/>
                         </svg>
